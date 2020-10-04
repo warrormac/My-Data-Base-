36652caaf2789fa05ef13ec6c4af3448233262e9
andre <andre.alvarez@ucsp.edu.pe>
2020-10-03 22:19:32 -0500

    Update Base de datos.cpp

--- Base de datos.cpp
+++ Base de datos.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
-#include <fstream>
-#include <string>
 #include <list>
+#include <string>
+#include <sstream>
+#include <fstream>
 
 using namespace std;
+
 
 string C1 = "CREAR_TABLA";
 string C2 = "INSERTAR";
@@ -11,9 +13,21 @@ string C4 = "SELECT_*_DESDE";
 string C3 = "BORRAR DESDE";
 string C5 = "MODIFICAR";
 
+//declaro la lista
+list<string> temp;
 
 
+void removespaceWord(string str)
+{
+    istringstream ss(str);
 
+    while (ss)
+    {
+        string word;
+        ss >> word;
+        temp.push_back(word);
+    }
+}
 
 bool fileExists(const std::string& filename)
 {
@@ -25,6 +39,7 @@ bool fileExists(const std::string& filename)
     return false;
 }
 
+
 void create()
 {
 
@@ -32,82 +47,84 @@ void create()
     //los CREAR_TABLA "nombre"(...)
 
     bool temp = 0;
-    temp=fileExists("STAT"); //verifica si ya existe es nombre
+    temp = fileExists("prueba1"); //verifica si ya existe es nombre
 
     if (temp == 0);
     else {
         string prueba;
 
-        ofstream MyReadFile("prueba.csv");
+        ofstream MyReadFile("prueba1.csv");
 
-        MyReadFile << "pruebas";
+        MyReadFile << "probando 1 ";
         // Close the file
         MyReadFile.close();
     }
 }
 
-//falta el comando insertar
 
-//falta el comando select
-
-//falta el comando borrar
-
-//falta el comando modificar
-
-
-
-void comads(string x)
+void commands(list<string> x)
 {
 
-    if (C1.compare(x) == 0)
+    if (C1 == (x.front()))
     {
         create();
         cout << "crea\n";
     }
-    else if (C2.compare(x) == 0)
+    else if (C2.compare(x.front()) == 0)
     {
         //create();
         cout << "insert\n";
     }
-    else if (C3.compare(x) == 0)
+    else if (C3.compare(x.front()) == 0)
     {
         //create();
         cout << "del\n";
     }
-    else if (C4.compare(x) == 0)
+    else if (C4.compare(x.front()) == 0)
     {
         //create();
         cout << "select\n";
     }
-    else if (C5.compare(x) == 0)
+    else if (C5.compare(x.front()) == 0)
     {
         //create();
         cout << "mod\n";
     }
     else { cout << "SYNTAX_ERROR: COMAND NOT FOUND"; }
-    
+
 
 }
 
 
 int main()
 {
-    string myText;
+    string x;
+
     
-    
-     //este es el IDLE
+
     while (true)
     {
-        string x;
-        cin >> x;
-        comads(x);
+        cout << "ingrese comando: ";
+
+        //creo ingreso el texto *COMANDO*. se hace de esta forma para poder 
+        //usar espacios
+        getline(cin, x);
+
+        //funcion que saca los espacios
+        removespaceWord(x);
+        //cout << temp.front();
+        commands(temp);
+        
+        //creo un iterador para poder imprimir la pablabra   **solo para probar (tester)** 
+        /*
+        list<string>::iterator itr;
+        for (itr = temp.begin(); itr != temp.end(); itr++)
+            cout << *itr << "\n";
+        */
+
     }
-
     
-
-    
-
-
-
-
 }
+
+
+
