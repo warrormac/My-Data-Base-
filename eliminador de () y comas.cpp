7f488688d27b277cd2e54386007fc1b82a9e95c4
andre <andre.alvarez@ucsp.edu.pe>
2020-10-04 14:24:20 -0500

    Update eliminador de () y comas.cpp

--- eliminador de () y comas.cpp
+++ eliminador de () y comas.cpp
@@ -1,20 +1,62 @@
-#include<iostream>
-#include<algorithm>
+#include <iostream>
+#include <list>
 #include <string>
 #include <sstream>
+#include <fstream>
 
 using namespace std;
 
-int main() 
+
+list<string> temp;
+
+
+void removespaceWord(string str)
 {
-    string my_str; ;
+    //elimina los "()" al ingresarlos 
+    str.erase(remove(str.begin(), str.end(), '('), str.end());
+    str.erase(remove(str.begin(), str.end(), ')'), str.end());
 
-    getline(cin, my_str);
-    cout << "string ingresado: " << my_str << endl;
+    //elimina la "," y lo convierte en " "
+    for (int i = 0; i < str.length(); i++)
+    {
+        if (str[i] == ',')
+            str[i] = ' ';
+    }
 
-    my_str.erase(remove(my_str.begin(), my_str.end(), '('), my_str.end()); 
-    my_str.erase(remove(my_str.begin(), my_str.end(), ','), my_str.end()); 
-    my_str.erase(remove(my_str.begin(), my_str.end(), ')'), my_str.end()); 
 
-    cout << "string despues de eliminar: " << my_str;
+
+    istringstream ss(str);
+
+    //ingreso el comando a la lista
+    while (ss)
+    {
+        string word;
+        ss >> word;
+        temp.push_back(word);
+    }
+}
+
+
+
+int main()
+{
+    string x;
+
+        cout << "ingrese comando: ";
+
+        //creo ingreso el texto *COMANDO*. se hace de esta forma para poder 
+        //usar espacios
+        getline(cin, x);
+
+        //funcion que saca los espacios
+        removespaceWord(x);
+        //cout << temp.front();
+        //commands(temp);
+
+        //creo un iterador para poder imprimir la pablabra   **solo para probar (tester)** 
+        
+        list<string>::iterator itr;
+        for (itr = temp.begin(); itr != temp.end(); itr++)
+            cout << *itr << "\n";
+
 }
